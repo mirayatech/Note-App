bebdf624ed22408948b70c637d4782872491f1d5
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-20 18:53:57 +0200

    create sidebar layout

--- note-react/src/App.js
+++ note-react/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./style/App.css";
+import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
---
+++ note-react/src/components/Sidebar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button>Add</button>
+      </div>
+      <div className="app-sidebar-notes">
+        <div className="app-sidebar-note ">
+          <div className="sidebar-note-title">
+            <strong>Title</strong>
+            <button>Delete</button>
+          </div>
+
+          <p>Note Preview</p>
+          <small className="note-meta">Last modified [date]</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
