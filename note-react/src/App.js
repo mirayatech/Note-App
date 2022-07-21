a2df10ee00723063ddd314cfb8388db87172bfeb
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-07-21 08:18:37 +0200

    adding note (layout)

--- note-react/src/App.js
+++ note-react/src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import uuid from "react-uuid";
 import "./style/App.css";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  // Notes
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar notes={notes} onAddNote={onAddNote} />
       <Main />
     </div>
   );
--- note-react/src/components/Sidebar.js
+++ note-react/src/components/Sidebar.js
@@ -1,22 +1,27 @@
 import React from "react";
 
-function Sidebar() {
+function Sidebar({ notes, onAddNote }) {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>Notes</h1>
-        <button>Add</button>
+        <button onClick={onAddNote}>Add</button>
       </div>
       <div className="app-sidebar-notes">
-        <div className="app-sidebar-note ">
-          <div className="sidebar-note-title">
-            <strong>Title</strong>
-            <button>Delete</button>
-          </div>
+        {/* Return Note when click on button */}
+        {notes.map((note) => (
+          <div className="app-sidebar-note ">
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button>Delete</button>
+            </div>
 
-          <p>Note Preview</p>
-          <small className="note-meta">Last modified [date]</small>
-        </div>
+            <p>{note.body}</p>
+            <small className="note-meta">
+              Last modified {note.lastModified}
+            </small>
+          </div>
+        ))}
       </div>
     </div>
   );
